43030d6e0109515c6c6c26f2feee633bb75c07da
unknown <s223371199@deakin.edu.au>
2023-07-25 13:31:23 +1000

    first commit

---
+++ myfirstjavascript.js
@@ -0,0 +1,12 @@
+console.log("Hello Javascript!");
+
+let variable1 = "string Value";
+let variable2 = 10;
+let variable3 = 13;
+let variableBoolean = true;
+
+console.log(variable1);
+//do same for other variables
+
+console.log(variable2 + variable3);
+//Do same for other variables...
