4878ed193521ce3d3dc75590b64b995d1540a705
s223371199 <139113989+s223371199@users.noreply.github.com>
2023-08-22 20:17:09 +1000

    Add files via upload
    2.1C

---
+++ ConceptsView.vue
@@ -0,0 +1,52 @@
+<script setup> 
+import { ref } from 'vue'
+
+const text = ref (' Edit me ')
+
+</script>
+<template>
+
+
+
+
+
+<div class = "myDiv">
+<h1> Text Input </h1>
+<input v-model= "text"> {{ text }}
+<h2> Checkbox</h2>
+<input type="checkbox" id="checkbox" v-model="checked">
+<label for="checkbox"> Checked: {{ checked }}</label>
+
+
+
+
+</div>
+
+</template>
+
+
+
+
+<style scoped>
+    .myDiv {
+    border: 5px outset lightgrey;
+    background-color: greenyellow;
+    text-align: center;
+    padding: 60px;
+    }
+
+
+</style>
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
---
+++ ExamplePage.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+
+const itemId = ref(0)
+const shoppingitem = ref('')
+const shoppinglist = ref([])
+const shopped = ref(false)
+
+function addItem() {
+  shoppinglist.value.push({
+    id: itemId.value++,
+    item: shoppingitem.value,
+    status: shopped.value
+  })
+  shoppingitem.value = ''
+}
+
+function removeItem(id) {
+  shoppinglist.value = shoppinglist.value.filter(item => item.id !== id)
+}
+</script>
+
+<template>
+  <div class="myDiv">
+    <h1>My Shopping list</h1>
+    <input type="text" v-model="shoppingitem">
+    <button @click="addItem">Add Item</button>
+
+    <ol>
+      <li v-for="(item, index) in shoppinglist" :key="item.id">
+        <input type="checkbox" v-model="item.status">
+        <span :class="{ done: item.status }">{{ item.item }}</span>
+        <button @click="removeItem(item.id)">X</button>
+      </li>
+    </ol>
+
+    <h1 v-if="shoppinglist.length < 1">No items</h1>
+  </div>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+.myDiv {
+  border: 5px outset lightgrey;
+  background-color: greenyellow;
+  text-align: center;
+  padding: 60px;
+}
+</style>
+
