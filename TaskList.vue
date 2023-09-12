85f745a295d8d1e15cc7169c56db1055ac9d847b
s223371199 <139113989+s223371199@users.noreply.github.com>
2023-09-12 13:07:00 +1000

    Add files via upload
    3.1D

---
+++ TaskList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>Tasks</h2>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">{{ task.title }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['tasks'],
+  };
+  </script>
+  
--- app.js
+++ app.js
@@ -1,57 +1,106 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        goals: [],
-        newGoal: {
-          title: '',
-          description: '',
-          deadline: '',
-        },
-        learningActivities: [],
+  data() {
+    return {
+      goals: [],
+      newGoal: {
+        title: '',
+        description: '',
+        deadline: '',
+      },
+      tasks: [],
+      newTask: {
+        title: '',
+        dueDate: '',
+      },
+    };
+  },
+  computed: {
+    totalLearningHours() {
+      return this.learningActivities.reduce((total, activity) => total + activity.hours, 0);
+    },
+  },
+  methods: {
+    addGoal() {
+      this.goals.push({ ...this.newGoal });
+      this.newGoal = {
+        title: '',
+        description: '',
+        deadline: '',
       };
     },
-    computed: {
-      totalLearningHours() {
-        return this.learningActivities.reduce((total, activity) => total + activity.hours, 0);
-      },
-    },
-    methods: {
-      addGoal() {
-        this.goals.push({ ...this.newGoal });
-        this.newGoal = {
+    addTask() {
+      if (this.newTask.title && this.newTask.dueDate) {
+        this.tasks.push({ id: Date.now(), ...this.newTask });
+        this.newTask = {
           title: '',
-          description: '',
-          deadline: '',
+          dueDate: '',
         };
-      },
-      addLearningActivity() {
-        // Logic to add a new learning activity
-      },
-      // Other methods
+        this.updateTaskPieChart();
+      }
     },
-    mounted() {
-      // Create a sample progress chart using Chart.js
-      const ctx = document.getElementById('progressChart').getContext('2d');
-      new Chart(ctx, {
-        type: 'line',
+    // Other methods
+
+    updateTaskPieChart() {
+      // Logic to update the task pie chart
+      // You can use Chart.js to create and update the pie chart here
+      const taskPieChartCanvas = document.getElementById('taskPieChart').getContext('2d');
+      const taskData = this.tasks.reduce((acc, task) => {
+        acc[task.title] = acc[task.title] ? acc[task.title] + 1 : 1;
+        return acc;
+      }, {});
+      const taskLabels = Object.keys(taskData);
+      const taskCounts = Object.values(taskData);
+
+      new Chart(taskPieChartCanvas, {
+        type: 'pie',
         data: {
-          labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+          labels: taskLabels,
           datasets: [
             {
-              label: 'Learning Hours',
-              data: [5, 10, 12, 20],
-              borderColor: '#3498db',
-              backgroundColor: 'rgba(52, 152, 219, 0.2)',
+              data: taskCounts,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)',
+                'rgba(255, 159, 64, 0.6)',
+              ],
             },
           ],
         },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-        },
       });
+
+      // Update total goals and total learning hours
+      document.getElementById('totalGoals').textContent = this.goals.length;
+      document.getElementById('totalLearningHours').textContent = this.totalLearningHours;
     },
-  });
-  
-  app.mount('#app');
-  
+  },
+  mounted() {
+    // Create a sample progress chart using Chart.js
+    const ctx = document.getElementById('progressChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+        datasets: [
+          {
+            label: 'Learning Hours',
+            data: [5, 10, 12, 20],
+            borderColor: '#3498db',
+            backgroundColor: 'rgba(52, 152, 219, 0.2)',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+
+    // Create the initial task pie chart
+    this.updateTaskPieChart();
+  },
+});
+
+app.mount('#app');
